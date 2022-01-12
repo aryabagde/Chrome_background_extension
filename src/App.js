67d85565e4f94df0a087904e1cb06e3a4c3d657f
aryabagde <akruyhau@gmail.com>
2022-01-11 21:40:12 -0800

    Initial_files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Menu from './pages/Menu';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'; // to route to different pages 
+                                                                          // to make sure we route 1 page at a time we are using Routes
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+        </Routes>
+        <Footer />
+      </Router>
+      
+
+    </div>
+  );
+}
+
+export default App;
